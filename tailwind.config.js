7058a0d380c1c34596d7fe9fd96abd4ca7f476dd
Nathan <nthn.wei@gmail.com>
2022-07-26 01:06:54 +0000

    Added custom text-shadow plugin by @itsmarkmead and refined prose coloring

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   // darkMode: 'class',
@@ -18,6 +19,11 @@ module.exports = {
       },
       screens: {
         xs: '868px',
+      },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
       },
       typography: ({ theme }) => ({
         DEFAULT: {
@@ -52,6 +58,16 @@ module.exports = {
     }
   },
   plugins: [
-    require('@tailwindcss/typography')
+    require('@tailwindcss/typography'),
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
   ],
 }
